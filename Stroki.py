524c0c6662537bd9a8e23f866c49a8961c1f6af5
Kostya2702 <loshkarev.kostya27@gmail.com>
2020-11-07 22:11:25 +0500

    Add files via upload

---
+++ Stroki.py
@@ -0,0 +1,11 @@
+
+
+
+def stroki():
+    Programming_language = input('Введите название языка: ')
+    today = input('Когда он будет самым популярным?: ')
+    stroka = f'{Programming_language} is the most popular programming language {today}'
+    print(stroka)
+
+stroki()
+
